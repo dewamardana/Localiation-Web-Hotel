0707cd1b2d472f9788b377d6d3d160f3b9da734b
dewamardana <dodekmardana33@gmail.com>
2025-07-01 22:40:08 +0800

    first commit

---
+++ en/dashboardCleaningDetail.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+    'title'            => 'Cleaning Data Dashboard',
+    'filter_start'     => 'Start Date:',
+    'filter_end'       => 'End Date:',
+    'filter_building'  => 'Building:',
+    'filter_all'       => 'All Building',
+    'filter_button'    => 'Filter',
+    'filter_reset'     => 'Reset',
+    'export_button'    => 'Export Excel',
+
+    'table' => [
+        'date'      => 'Date',
+        'name'      => 'Name',
+        'oa'        => 'OA',
+        'ov'        => 'OV',
+        'stay'      => 'Stay',
+        'vec'       => 'Vec',
+        'premier'   => 'Premier',
+        'total'     => 'Total',
+        'point'     => 'Point',
+        'total_info'=> 'Total',
+        'member'=> 'member',
+    ],
+
+    'members' => 'Member(s)',
+];
